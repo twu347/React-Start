3315470be139b748e184cd0aa961082cade8d159
吴桐 <twu347@uwo.ca>
2022-12-19 21:57:44 -0500

    React Component
    Make HTML display based on multiple components --1:18:00

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,28 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // the component that will return HTML user interface, the component must start with capital letter
-function TemporaryName() {
+function ParentComponent() {
     return(
         <div>
-            <header>
-                <nav>This is the Header</nav>
-            </header>
+            <Header/>
             <h1>Skycore Software & Co.</h1>
             <h3>Our Goal: Produce High Quality Custom Software</h3>
-            <ul>
-                <li>Home</li>
-                <li>Pricing</li>
-                <li>About Us</li>
-                <li>Current Projects</li>
-                <li>Our Mission</li>
-            </ul>
-            <footer>
-                2022 Skycore Software & Co. All Rights Reserved 
-            </footer>
+            <NavBar/>
+            <Footer/>
         </div>
     )
 };
 
-// <TemporaryName/> is the way to define component in React, where it is similar to an HTML tag
-ReactDOM.render(<TemporaryName/>, document.getElementById("root"));
+// header component
+function Header(){
+    return(
+        <header>
+            <nav>This is the header </nav>
+        </header>
+    )
+};
 
+// footer component
+function Footer(){
+    return(
+        <footer>
+            2022 Skycore Software & Co. All Rights Reserved 
+        </footer>
+    )
+};
+
+// navigation bar component 
+function NavBar(){
+    return(
+        <ul>
+            <li>Home</li>
+            <li>Pricing</li>
+            <li>About Us</li>
+            <li>Current Projects</li>
+            <li>Our Mission</li>
+        </ul>
+    )
+};
+
+// <ParentComponent/> is the way to define component in React, where it is similar to an HTML tag
+ReactDOM.render(<ParentComponent/>, document.getElementById("root"));
