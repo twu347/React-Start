0021f1e1669c5e1b6d38e050c5d20d7072d1820d
吴桐 <twu347@uwo.ca>
2022-12-19 21:46:53 -0500

    React Component
    Create a basic component --1:14:00

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,45 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// // use ReactDOM.render() to create a header line
-// const element = <h1>Hello World -- React</h1>;
-// ReactDOM.render(element, document.getElementById('root'));
+// the component that will return HTML user interface, the component must start with capital letter
+function TemporaryName() {
+    return(
+        <div>
+            <header>
+                <nav>This is the Header</nav>
+            </header>
+            <h1>Skycore Software & Co.</h1>
+            <h3>Our Goal: Produce High Quality Custom Software</h3>
+            <ul>
+                <li>Home</li>
+                <li>Pricing</li>
+                <li>About Us</li>
+                <li>Current Projects</li>
+                <li>Our Mission</li>
+            </ul>
+            <footer>
+                2022 Skycore Software & Co. All Rights Reserved 
+            </footer>
+        </div>
+    )
+};
 
-// // recreate the above line of code in vanilla JS - dynamic programming 
-// const h1 = document.createElement("h1");
-// h1.textContent = "Dynamic Programming Header"; 
-// h1.className = "header"; 
-// document.getElementById("root").append(h1);
+// <TemporaryName/> is the way to define component in React, where it is similar to an HTML tag
+ReactDOM.render(<TemporaryName/>, document.getElementById("root"));
 
-// // JSX 
-// const page = <div><h1> Inside DIV </h1></div>
-// ReactDOM.render(page,document.getElementById("root"));
-
-// const navbar = (
-//     <nav>
-//         <h1>Skycore Software & Co.</h1>
-//         <ul>
-//             <li> About Us </li>
-//             <li> Current Projects </li>
-//             <li> Our Mission </li>
-//         </ul>
-//     </nav>
-// );
-// ReactDOM.render(navbar, document.getElementById("root"));
-
-const page = (
-    <div>
-        <h1>Skycore Software & Co.</h1>
-        <h3>Our Goal: Produce High Quality Custom Software</h3>
-        <ul>
-            <li>Home</li>
-            <li>Pricing</li>
-            <li>About Us</li>
-            <li>Current Projects</li>
-            <li>Our Mission</li>
-        </ul>
-    </div>
-);
-// This will output javascript object due to .append()
-// document.getElementById("root").append(JSON.stringify(page)); 
-ReactDOM.render(page, document.getElementById("root"));
