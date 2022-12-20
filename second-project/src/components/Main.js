e46dbee6d781b077e5081500b6f1a835fdce278a
吴桐 <twu347@uwo.ca>
2022-12-20 12:43:31 -0500

    Complete Main Component
    Finish project --2:20:00

--- second-project/src/components/Main.js
+++ second-project/src/components/Main.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function MainHeader(){
     return(
-        <h1>Hello World - Main Component </h1>
+        <main>
+            <h1 className="main-header">Technologies We Used</h1>
+            <ul className="main-ul">
+                <li>Front End: Angular, React</li>
+                <li>Back End: Node.js, Express.js</li>
+                <li>Database: MySQL, MongoDB</li>
+                <li>Operating System: MacOS, Linux, Windows</li>
+                <li>Web Server: Amazon Web Services, Digital Ocean</li>
+            </ul>
+        </main>
     )
 };
 
